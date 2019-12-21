0e6ebe7ad54376c07e742aa60dfee0f17d2e8429
Adrian Good <aacgood@gmail.com>
2019-12-22 10:38:41 +1100

    Updated to remove external dependencies, changed to ES6.x

--- Vagrantfile
+++ Vagrantfile
@@ -12,7 +12,7 @@ Vagrant.configure("2") do |config|
   config.vm.provision :shell, path: "bootstrap.sh"
   config.vm.network "forwarded_port", guest: 9000, host: 9000, host_ip: "127.0.0.1"
   config.vm.network "forwarded_port", guest: 9001, host: 9001, host_ip: "127.0.0.1"
-  config.vm.synced_folder "./build_files", "/vagrant"
+
 
   config.vm.provider "virtualbox" do |vb, override|
     vb.gui = true
@@ -27,34 +27,4 @@ Vagrant.configure("2") do |config|
   end
 
 
-  # Disable automatic box update checking. If you disable this, then
-  # boxes will only be checked for updates when the user runs
-  # `vagrant box outdated`. This is not recommended.
-  # config.vm.box_check_update = false
-
-  # Create a forwarded port mapping which allows access to a specific port
-  # within the machine from a port on the host machine. In the example below,
-  # accessing "localhost:8080" will access port 80 on the guest machine.
-  # NOTE: This will enable public access to the opened port
-  # config.vm.network "forwarded_port", guest: 80, host: 8080
-
-  # Create a forwarded port mapping which allows access to a specific port
-  # within the machine from a port on the host machine and only allow access
-  # via 127.0.0.1 to disable public access
-  # config.vm.network "forwarded_port", guest: 80, host: 8080, host_ip: "127.0.0.1"
-
-  # Create a private network, which allows host-only access to the machine
-  # using a specific IP.
-  # config.vm.network "private_network", ip: "192.168.33.10"
-
-  # Create a public network, which generally matched to bridged network.
-  # Bridged networks make the machine appear as another physical device on
-  # your network.
-  # config.vm.network "public_network"
-
-  # Share an additional folder to the guest VM. The first argument is
-  # the path on the host to the actual folder. The second argument is
-  # the path on the guest to mount the folder. And the optional third
-  # argument is a set of non-required options.
-  # config.vm.synced_folder "../data", "/vagrant_data"
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,29 +1,57 @@
 #! /bin/bash
 
 export DEBIAN_FRONTEND=noninteractive
+THEHIVE_URL="http://127.0.0.1:9000"
+CORTEX_URL="http://127.0.0.1:9001"
+LOG_FILE=$(mktemp)
+
+check() {
+    expected=$1
+    shift
+    status_code=$(curl -v "$@" -s -o /dev/stderr -w '%{http_code}' 2>>${LOG_FILE}) || true
+    if [ "${status_code}" = "${expected}" ]
+    then
+      ok
+    else
+      ko
+      echo "got ${status_code}, expected ${expected}" >&2
+      echo "see more detail in $LOG_FILE" >&2
+      exit 1
+    fi
+}
+
+ok() {
+    echo "OK" >&2
+}
+
+ko() {
+    echo "KO" >&2
+}
+
+
 
 system_update(){
     echo "[$(date +%H:%M:%S)]: Running apt-get update..."
-    apt-get update    
+    apt-get update -y -qq   
     
     echo "[$(date +%H:%M:%S)]: Running apt-get upgrade..."
-    apt-get upgrade -y
+    apt-get upgrade -y -qq
 
     echo "[$(date +%H:%M:%S)]: Download Pre-requsites..."
-    apt-get install zip unzip -y
-    apt-get install python3-pip -y 
+    apt-get install zip unzip -y -qq
+    apt-get install python3-pip -y -qq
 }
 
 install_openjdk(){
     echo "[$(date +%H:%M:%S)]: Installing OpenJDK"
     add-apt-repository ppa:openjdk-r/ppa -y
-    apt-get install openjdk-8-jre-headless -y
+    apt-get install openjdk-8-jre-headless -y -qq
 }
 
 install_elastic(){
-    echo "[$(date +%H:%M:%S)]: Installing Elasticsearch 5.x"    
+    echo "[$(date +%H:%M:%S)]: Installing Elasticsearch 6.x"
     apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key D88E42B4 >/dev/null
-    bash -c 'echo deb https://artifacts.elastic.co/packages/5.x/apt stable main > /etc/apt/sources.list.d/elastic-5.x.list'
+    bash -c 'echo deb https://artifacts.elastic.co/packages/6.x/apt stable main > /etc/apt/sources.list.d/elastic-6.x.list'
     apt-get install apt-transport-https -y
     apt-get update && apt-get install elasticsearch -y
 }
@@ -32,7 +60,6 @@ configure_elastic(){
     echo "[$(date +%H:%M:%S)]: Updating elasticsearch.yml file"    
     sed -i 's/#network.host: 192.168.0.1/network.host: 127.0.0.1/g' /etc/elasticsearch/elasticsearch.yml
     sed -i 's/#cluster.name: my-application/cluster.name: hive/g' /etc/elasticsearch/elasticsearch.yml
-    bash -c 'echo script.inline: true >> /etc/elasticsearch/elasticsearch.yml'
     bash -c 'echo thread_pool.index.queue_size: 100000 >> /etc/elasticsearch/elasticsearch.yml'
     bash -c 'echo thread_pool.search.queue_size: 100000 >> /etc/elasticsearch/elasticsearch.yml'
     bash -c 'echo thread_pool.bulk.queue_size: 100000 >> /etc/elasticsearch/elasticsearch.yml'   
@@ -44,6 +71,24 @@ configure_elastic(){
     systemctl restart elasticsearch.service
 }
 
+install_docker(){
+    echo "[$(date +%H:%M:%S)]: Installing Docker"
+    wget -O- https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - >/dev/null 2>&1
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+    apt-get install docker-ce -y -qq
+}
+
+install_cortex(){
+    echo "[$(date +%H:%M:%S)]: Downloading Cortex v3.0.0-1"
+    cd /opt 
+    wget https://dl.bintray.com/thehive-project/binary/cortex-3.0.0-1.zip >/dev/null 2>&1
+    wget http://dl.bintray.com/thehive-project/binary/cortex-3.0.0-1.zip.asc  >/dev/null 2>&1
+    gpg --verify cortex-3.0.0-1.zip.asc cortex-3.0.0-1.zip
+    # Need logic to confirm its ok.....
+    unzip -q cortex-3.0.0-1.zip
+    ln -s cortex-3.0.0-1 cortex
+}
+
 install_thehive(){
     echo "[$(date +%H:%M:%S)]: Downloading TheHive v3.4.0-1"   
     cd /opt 
@@ -53,46 +98,16 @@ install_thehive(){
     gpg --import PGP-PUBLIC-KEY
     gpg --verify thehive-3.4.0-1.zip.asc thehive-3.4.0-1.zip
     # Need logic to confirm its ok.....
-    unzip thehive-3.4.0-1.zip
+    unzip -q thehive-3.4.0-1.zip
     ln -s thehive-3.4.0-1 thehive
 }
 
-configure_thehive(){
-    echo "[$(date +%H:%M:%S)]: Adding play.secret to TheHive application.conf file"
-    (cat << _EOF_
-# Secret key
-# ~~~~~
-# The secret key is used to secure cryptographics functions.
-# If you deploy your application to several instances be sure to use the same key!
-play.http.secret.key="$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)"
-_EOF_
-) | sudo tee -a /opt/thehive/conf/application.conf >/dev/null 2>&1
-    
-    echo "[$(date +%H:%M:%S)]: Configuring TheHive service"
-    sed -i 's/\/etc\/thehive\//\/opt\/thehive\/conf\//g' /opt/thehive/package/thehive.service
-
-    mkdir /var/log/thehive
-    addgroup thehive
-    adduser --system thehive
-    cp /opt/thehive/package/thehive.service /usr/lib/systemd/system
-    chown -R thehive:thehive /opt/thehive
-    chown -R thehive:thehive /opt/thehive-3.4.0-1
-    chown -R thehive:thehive /var/log/thehive
-    chgrp thehive /opt/thehive/conf/application.conf
-    chmod 640 /opt/thehive/conf/application.conf
-    systemctl enable thehive
-    service thehive start
-}
-
-install_cortex(){
-    echo "[$(date +%H:%M:%S)]: Downloading Cortex v3.0.0-1"
-    cd /opt 
-    wget https://dl.bintray.com/thehive-project/binary/cortex-3.0.0-1.zip >/dev/null 2>&1
-    wget http://dl.bintray.com/thehive-project/binary/cortex-3.0.0-1.zip.asc  >/dev/null 2>&1
-    gpg --verify thehive-3.4.0-1.zip.asc thehive-3.4.0-1.zip
-    # Need logic to confirm its ok.....
-    unzip cortex-3.0.0-1.zip
-    ln -s cortex-3.0.0-1 cortex
+install_python_utils(){
+    # pip3 list --format=columns | grep thehive4py 
+    echo "[$(date +%H:%M:%S)]: Installaing thehive4py/cortex4py"
+    pip3 install cortexutils
+    pip3 install thehive4py
+    pip3 install cortex4py
 }
 
 configure_cortex(){
@@ -111,8 +126,10 @@ _EOF_
 ) | sudo tee -a /opt/cortex/conf/application.conf >/dev/null 2>&1
 
     echo "[$(date +%H:%M:%S)]: Configuring Cortex service"
+    sed -i 's/#play.modules.enabled += connectors.cortex.CortexConnector/play.modules.enabled += connectors.cortex.CortexConnector/g' /opt/thehive/conf/application.conf
+    sed -i 's/auth.method.basic = false/auth.method.basic = true/g' /opt/cortex/conf/application.conf
     sed -i 's/\/etc\/cortex\//\/opt\/cortex\/conf\//g' /opt/cortex/package/cortex.service
-
+    
     mkdir /var/log/cortex
     addgroup cortex
     adduser --system cortex
@@ -122,70 +139,146 @@ _EOF_
     chown -R cortex:cortex /var/log/cortex
     chgrp cortex /opt/cortex/conf/application.conf
     chmod 640 /opt/cortex/conf/application.conf
+    usermod -a -G docker cortex
     systemctl enable cortex
     service cortex start
 }
 
-
-add_analyzers(){
-    echo "[$(date +%H:%M:%S)]: Installing Docker"
-    wget -O- https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - >/dev/null 2>&1
-    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-    apt-get install docker-ce -y
-    usermod -a -G docker cortex
-
-    echo "[$(date +%H:%M:%S)]: Configuring Cortex Analyzers and Responders"
+configure_thehive(){
+    echo "[$(date +%H:%M:%S)]: Adding play.secret to TheHive application.conf file"
     (cat << _EOF_
-
-job {
-    runner = [docker]
-}
+# Secret key
+# ~~~~~
+# The secret key is used to secure cryptographics functions.
+# If you deploy your application to several instances be sure to use the same key!
+play.http.secret.key="$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)"
 _EOF_
-) | sudo tee -a /opt/cortex/conf/application.conf >/dev/null 2>&1
+) | sudo tee -a /opt/thehive/conf/application.conf >/dev/null 2>&1
+    
+    echo "[$(date +%H:%M:%S)]: Configuring TheHive service"
+    sed -i 's/\/etc\/thehive\//\/opt\/thehive\/conf\//g' /opt/thehive/package/thehive.service
+    sed -i 's/auth.method.basic = false/auth.method.basic = true/g' /opt/thehive/conf/application.conf
 
+    mkdir /var/log/thehive
+    addgroup thehive
+    adduser --system thehive
+    cp /opt/thehive/package/thehive.service /usr/lib/systemd/system
+    chown -R thehive:thehive /opt/thehive
+    chown -R thehive:thehive /opt/thehive-3.4.0-1
+    chown -R thehive:thehive /var/log/thehive
+    chgrp thehive /opt/thehive/conf/application.conf
+    chmod 640 /opt/thehive/conf/application.conf
+    systemctl enable thehive
+    service thehive start
+}
+
+init_cortex(){
+    echo "[$(date +%H:%M:%S)]: Checking Cortex is available"
+    sleep 30
+    check 200 "$CORTEX_URL/index.html"
+
+    echo "[$(date +%H:%M:%S)]: Creating the index"
+    check 204 -XPOST "$CORTEX_URL/api/maintenance/migrate"
+
+    echo "[$(date +%H:%M:%S)]: Creating the Cortex SuperAdmin account"
+    check 201 "$CORTEX_URL/api/user" -H 'Content-Type: application/json' -d '
+            {
+              "login" : "admin",
+              "name" : "admin",
+              "roles" : [
+                  "superadmin"
+               ],
+              "preferences" : "{}",
+              "password" : "thehive1234",
+              "organization": "cortex"
+            }'
+
+    echo "[$(date +%H:%M:%S)]: Creating the Cortex 'YourOrg' Organization"
+    check 201 -u admin:thehive1234 "$CORTEX_URL/api/organization" -H 'Content-Type: application/json' -d '
+        {
+          "name": "YourOrg",
+          "description": "Your organization"
+        }'
+
+
+    echo "[$(date +%H:%M:%S)]: Creating the Cortex 'YourOrg' OrgAdmin"
+    check 201 -u admin:thehive1234 "$CORTEX_URL/api/user" -H 'Content-Type: application/json'  -d '
+        {
+          "login" : "thehive",
+          "name" : "thehive",
+          "roles" : [
+              "read",
+              "analyze",
+              "orgadmin"
+           ],
+          "password" : "thehive1234",
+          "organization": "YourOrg"
+        }'
+
+    echo "[$(date +%H:%M:%S)]: Creating api key for OrgAdmin"
+    # API key that contains a '/' causes issues, so keep trying until we get one without a '/'
+    key="///" 
+
+    while true; do
+        sleep 1
+        if [[ "$key" == */* ]];
+        
+        then
+            key=$(curl -s -u admin:thehive1234 "$CORTEX_URL/api/user/thehive/key/renew" -d '')
+        else
+            break
+        fi
+    done
+
+    check 200 "$CORTEX_URL/api/user/thehive" -H 'Content-Type: application/json' \
+        -H "Authorization: Bearer $key"
+
+    
+    echo "[$(date +%H:%M:%S)]: Enabling TheHive / Cortex Integration Key"
+    sed -i 's/^cortex {.*/cortex {\n  "CORTEX" {\n    url = "http:\/\/127.0.0.1:9001"\n    key = '$key'\n    ws {}\n  }\n}/g' /opt/thehive/conf/application.conf
+    sed -i '/#"CORTEX-SERVER-ID" {/,+6 d' /opt/thehive/conf/application.conf
+
+    echo "[$(date +%H:%M:%S)]: Disabling basic auth for Cortex"
+    sed -i 's/auth.method.basic = true/auth.method.basic = false/g' /opt/cortex/conf/application.conf
+
+    echo "[$(date +%H:%M:%S)]: Configuring Docker Analyzers/Responders for Cortex"
     sed -i '0,/urls = \[\]/s//urls = \[\"https:\/\/dl.bintray.com\/thehive-project\/cortexneurons\/analyzers.json"\]/' /opt/cortex/conf/application.conf
     sed -i '0,/urls = \[\]/s//urls = \[\"https:\/\/dl.bintray.com\/thehive-project\/cortexneurons\/responders.json"\]/' /opt/cortex/conf/application.conf
 
-}
-
-restore_db(){
-    echo "[$(date +%H:%M:%S)]: Restoring TheHive DB"
-    cp /vagrant/thehivedb.zip /opt/backup
-    cd /opt/backup
-    unzip thehivedb.zip
-    chown -R elasticsearch:elasticsearch /opt/backup
-
-    echo "[$(date +%H:%M:%S)]: Registering snapshot with Elasticsearch"
-    curl -s -XPUT 'http://localhost:9200/_snapshot/the_hive_backup' -d '{
-        "type": "fs",
-        "settings": {
-            "location": "/opt/backup",
-            "compress": true
-        }
-    }' 
-
-    echo "[$(date +%H:%M:%S)]: Performing restore of TheHive and Cortex DB's"
-    curl -XPOST 'http://localhost:9200/_snapshot/the_hive_backup/backup/_restore' -d '{
-        "indices": [ "the_hive_15", "cortex_4"]
-    }' 
+    echo "[$(date +%H:%M:%S)]: Restarting TheHive and Cortex"
+    service thehive restart
+    service cortex restart
     sleep 30
-    curl -s 'http://localhost:9200/_cat/indices?v'
+    check 200 "$CORTEX_URL/index.html"    
 }
 
-integrate_thehive_cortex(){
-    echo "[$(date +%H:%M:%S)]: Enabling TheHive/Cortex integration"
-    sed -i 's/#play.modules.enabled += connectors.cortex.CortexConnector/play.modules.enabled += connectors.cortex.CortexConnector/g' /opt/thehive/conf/application.conf
-    sed -i 's/^cortex {.*/cortex {\n  "CORTEX" {\n    url = "http:\/\/127.0.0.1:9001"\n    key = "xYMjaioc7CCsTPkk2n\/Wt7msmZ36RFz8"\n    ws {}\n  }\n}/g' /opt/thehive/conf/application.conf
-    sed -i '/#"CORTEX-SERVER-ID" {/,+6 d' /opt/thehive/conf/application.conf
+activate_analyzer() {
+
+    echo "[$(date +%H:%M:%S)]: Activating Analyzer $1"
+    if [ "$2" ]
+    then
+      data="$2"
+    else  
+      data='{
+              "configuration": {
+                  "check_tlp": false,
+                  "max_tlp": 2
+              },
+              "name": '\"$1\"'
+          }'
+    fi
+
+    status_code=$(curl -s -H "Authorization: Bearer ${key}" "$CORTEX_URL/api/organization/analyzer/${1}" \
+        -H 'Content-Type: application/json' -d "$data" -o /dev/stderr -w '%{http_code}')
+
+    if [ "${status_code}" = "201" ]
     
-}
+    then
+        ok
+    else
+        ko
+    fi
 
-install_apiutils(){
-    # pip3 list --format=columns | grep thehive4py 
-    echo "[$(date +%H:%M:%S)]: Installaing thehive4py/cortex4py"
-    pip3 install cortexutils
-    pip3 install thehive4py
-    pip3 install cortex4py
 }
 
 configure_pre_logon(){
@@ -210,36 +303,54 @@ _EOF_
 
 }
 
-system_cleanup(){
-    echo "[$(date +%H:%M:%S)]: Performing clean up"
-    cd /opt
-    mkdir packages
-    chown thehive:thehive packages
-    mv *.zip* packages
-    mv PGP-PUBLIC-KEY packages
-}
-
 reboot_system(){
     echo "[$(date +%H:%M:%S)]: Script Complete - Rebooting"
+    sleep 30
     reboot now
 }
+
+
+# Script starts here
 
 main() {
     system_update
     install_openjdk
     install_elastic
     configure_elastic
-    install_thehive
-    configure_thehive
+    install_docker
     install_cortex
+    install_thehive
+    install_python_utils
     configure_cortex
-    add_analyzers
-    restore_db
-    integrate_thehive_cortex
-    install_apiutils
+    configure_thehive
+    init_cortex
+    activate_analyzer Abuse_Finder_2_0
+    activate_analyzer DShield_lookup_1_0
+    activate_analyzer FileInfo_7_0 '{ 
+    "name": "FileInfo_7_0",
+    "configuration": {
+        "manalyze_enable": false,
+        "manalyze_enable_docker": false,
+        "manalyze_enable_binary": false,
+        "auto_extract_artifacts": true,
+        "check_tlp": false,
+        "max_tlp": 2,
+        "check_pap": false,
+        "max_pap": 2
+    },
+    "jobCache": 0
+    }'
+    activate_analyzer EmlParser_1_2
+    activate_analyzer MaxMind_GeoIP_3_0
+    activate_analyzer UnshortenLink_1_2
+    activate_analyzer Fortiguard_URLCategory_2_1
+    activate_analyzer CyberCrime-Tracker_1_0
+    activate_analyzer TalosReputation_1_0
+    activate_analyzer URLhaus_2_0
+    activate_analyzer Urlscan_io_Search_0_1_0
     configure_pre_logon
-    system_cleanup
     reboot_system
+
 }
 
 main
